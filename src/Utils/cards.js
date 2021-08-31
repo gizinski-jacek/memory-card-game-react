f329ea6192dd2359837c9cc47d75d210bd137aea
mrjackmaster <jacektrg+github@gmail.com>
2021-08-31 20:26:51 +0200

    Array containing cards with id's, src to image and clicked boolean.

---
+++ src/Utils/cards.js
@@ -0,0 +1,79 @@
+const cards = [
+	{
+		id: 'card00',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card01',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card02',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card03',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card04',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card05',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card06',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card07',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card08',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card09',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card10',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card11',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card12',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card13',
+		src: '',
+		clicked: false,
+	},
+	{
+		id: 'card14',
+		src: '',
+		clicked: false,
+	},
+];
+
+export default cards;
